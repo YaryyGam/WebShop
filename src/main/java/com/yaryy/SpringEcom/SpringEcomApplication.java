4f4f42ceb477a0a22b1f3f237d3e237aa07b2942
Yaroslav <gamerplay0606@gmail.com>
2024-09-11 02:07:17 +0300

    Initial commit

---
+++ src/main/java/com/yaryy/SpringEcom/SpringEcomApplication.java
@@ -0,0 +1,13 @@
+package com.yaryy.SpringEcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEcomApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/yaryy/SpringEcom/controller/ProductController.java
@@ -0,0 +1,16 @@
+package com.yaryy.SpringEcom.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+    @GetMapping("/products")
+    public String getProducts(){
+        return "All Products";
+    }
+
+}
