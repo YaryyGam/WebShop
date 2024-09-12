ce016cb41b238ab8f309f5647df588e1123dba19
Yaroslav <gamerplay0606@gmail.com>
2024-09-12 16:38:14 +0300

    update

--- src/main/java/com/yaryy/SpringEcom/repo/ProductRepo.java
+++ src/main/java/com/yaryy/SpringEcom/repo/ProductRepo.java
@@ -3,6 +3,7 @@ package com.yaryy.SpringEcom.repo;
 import com.yaryy.SpringEcom.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,6 +16,6 @@ public interface ProductRepo extends JpaRepository<Product, Integer> {
             "LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
             "LOWER(p.brand) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
             "LOWER(p.category) LIKE LOWER(CONCAT('%', :keyword, '%'))")
-    List<Product> searchProducts(String keyword);
+    List<Product> searchProducts(@Param("keyword") String keyword);
 
 }
